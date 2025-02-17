5a679e14e54d155ddd6e1ef5c08a7b7140992d01
Yavuz <yvzerv@gmail.com>
2025-02-17 16:11:05 +0300

    Yeni proje denemesi.

---
+++ src/main/java/org/deneme/studentsbackend/StudentsBackendApplication.java
@@ -0,0 +1,13 @@
+package org.deneme.studentsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/deneme/studentsbackend/StudentsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.deneme.studentsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
